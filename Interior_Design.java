37b760f88e3a5b858041816eabbaab2cb0b36e37
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-01-11 12:02:01 +0530

    import java.util.Scanner; public class Vijaya {     public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     int a=sc.nextInt();     int b=sc.nextInt();     int c=sc.nextInt();     int d=sc.nextInt();     int e=a+b;     int f=c+d;     if(e>f)     {     System.out.printf("%d",f); } else {     System.out.printf("%d",e); } } }

---
+++ Interior_Design.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Vijaya
+{
+    public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    int d=sc.nextInt();
+    int e=a+b;
+    int f=c+d;
+    if(e>f)
+    {
+    System.out.printf("%d",f);
+}
+else
+{
+    System.out.printf("%d",e);
+}
+}
+}
