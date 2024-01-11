322c132a9fcf56bd359e93fa57d4935a9d0219e0
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-01-11 10:22:26 +0530

    import java.util.Scanner; public class Vijaya {     public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     int a=sc.nextInt();     int b;     int c;
        b=4*a;
        c=a*a;
        System.out.printf("%d %d",c,b);
    }
    }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Vijaya
+{
+    public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b;
+    int c;
+    
+    b=4*a;
+    c=a*a;
+    System.out.printf("%d %d",c,b);
+}
+}
