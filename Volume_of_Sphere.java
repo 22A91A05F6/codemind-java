3a6241add5d293475d2cecc14c3f46bc760535de
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-01-11 10:01:41 +0530

    import java.util.Scanner; public class Vijaya {     public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     int a=sc.nextInt();
        double p;
        p=(4*3.14*a*a*a)/3;
        System.out.printf("%.2f",p);
    }
    }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Vijaya
+{
+    public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+
+    double p;
+    p=(4*3.14*a*a*a)/3;
+    System.out.printf("%.2f",p);
+}
+}
