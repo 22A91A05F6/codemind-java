f46bf7ba695302a3c7ac3788d1d11d71ffa3bf6c
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-01-11 11:38:45 +0530

    import java.util.Scanner; public class Vijaya {     public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     int a=sc.nextInt();     int b=sc.nextInt();     int c=5*a;     int d=7*b;     System.out.printf("%d",c+d); } }

---
+++ King_Tours_.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Vijaya
+{
+    public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=5*a;
+    int d=7*b;
+    System.out.printf("%d",c+d);
+}
+}
