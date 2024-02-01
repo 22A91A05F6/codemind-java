9df2043aae04adb7f1b78e5fc277c804124e4818
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-02-01 10:12:04 +0530

    import java.util.Scanner; public class Vijaya{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int p=sc.nextInt();         int s=(p*(p-1))/2;         System.out.println(s);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Vijaya{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int s=(p*(p-1))/2;
+        System.out.println(s);
+    }
+}
