13f70cbb57eb0314f5a76ae8ce6494e12f17fb33
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-02-07 22:32:33 +0530

    import java.util.Scanner; public class Vijaya{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int p=sc.nextInt();         int k;         k=(p*(p+1))/2;         System.out.println(k);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Vijaya{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int k;
+        k=(p*(p+1))/2;
+        System.out.println(k);
+    }
+}
