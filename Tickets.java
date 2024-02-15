50c3352038bab7ea94a59d28ba84bb311652ff4f
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-02-15 09:33:33 +0530

    import java.util.Scanner; public class Vij{ public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     int p=sc.nextInt();     if((4*p)<=1000)     {         System.out.println("YES");     }     else     {         System.out.println("NO");     } } }

---
+++ Tickets.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Vij{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int p=sc.nextInt();
+    if((4*p)<=1000)
+    {
+        System.out.println("YES");
+    }
+    else
+    {
+        System.out.println("NO");
+    }
+}
+}
