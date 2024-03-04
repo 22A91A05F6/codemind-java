c7dbad788741c9d7ceecec455846cfc495e229b6
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-03-04 12:07:57 +0530

    import java.util.Scanner; public class Vij{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int p=sc.nextInt();         if(p%2==0)         {             System.out.println("1");         }         else         {             System.out.println("2");         }     } }

---
+++ IsOdd?.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Vij{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        if(p%2==0)
+        {
+            System.out.println("1");
+        }
+        else
+        {
+            System.out.println("2");
+        }
+    }
+}
