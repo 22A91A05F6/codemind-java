9550a036f79395823b808d3e397bcfc9a0b30175
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-03-05 10:14:12 +0530

    Add files via upload

---
+++ character-tostring.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Stch{
+    public static void main(String[] args)
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        
+            String s=""+'z';//convert char to string
+            String p=Character.toString('X');//2 ways 
+            System.out.println(s);
+            System.out.println(p);
+            /*String vi="hello";
+            char[] arr2=vi.toCharArray();
+            Arrays.sort(arr2);
+            String sortedstring=new String(arr2);
+            System.out.println(sortedstring);*/
+
+    }
+}
