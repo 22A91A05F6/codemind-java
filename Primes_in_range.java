6d9c4ec7c6240cb7fc96aa15679d3c58e2a392e4
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2024-03-06 09:43:59 +0530

    import java.util.Scanner;
    public class Vij{
        static boolean isprime(int k)
        {
            if(k<=1)
            {
                return false;
            }
            else
            {
            for(int j=2;j<=(int)Math.sqrt(k);j++)
            {
                if(k%j==0)
                {
                    return false;
                }
        
            }
            }
            return true;
            
        }
        public static void main(String[] args)
        {
            Scanner sc=new Scanner(System.in);
            
                int a=sc.nextInt();
                int b=sc.nextInt();
                int c=0;
                for(int i=a;i<=b;i++)
                {
                    if(isprime(i))
                    {
                        c++;
                    }
                }
            System.out.println(c);
        }
    }

--- Primes_in_range.java
+++ Primes_in_range.java
@@ -1,36 +1,40 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Vij{
-    public static boolean isprime(int n)
+    static boolean isprime(int k)
     {
-        if(n<=1)
+        if(k<=1)
         {
             return false;
         }
         else
         {
-            for(int j=2;j<=n/2;j++)
+        for(int j=2;j<=(int)Math.sqrt(k);j++)
+        {
+            if(k%j==0)
             {
-                if(n%j==0)
+                return false;
+            }
+    
+        }
+        }
+        return true;
+        
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=0;
+            for(int i=a;i<=b;i++)
+            {
+                if(isprime(i))
                 {
-                    return false;
+                    c++;
                 }
             }
-            return true;
-        }
-    }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=0;
-        for(int i=a;i<=b;i++)
-        {
-            if(isprime(i))
-            {
-                c+=1;
-            
-            }
-        }
         System.out.println(c);
     }
 }
